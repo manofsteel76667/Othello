55bdcb6ad85108c56767464f1dec4fe6c027442e
mike anderson <jmanderson07@comcast.net>
2015-03-23 21:13:33 -0400

    Commented out the part of DecisionNode that tried to calculate position strength by finding tiles that were impossible to lose.  Causing a null reference exception; look at it later.

--- src/DecisionNode.java
+++ src/DecisionNode.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
